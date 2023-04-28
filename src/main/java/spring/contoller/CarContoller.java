1f1a4f6faf42a2c7d43b3ab0bb281a8ee018e764
79510 <eduard-proskurin@mail.ru>
2023-04-28 19:44:37 +0300

    Worked json

--- src/main/java/spring/contoller/CarContoller.java
+++ src/main/java/spring/contoller/CarContoller.java
@@ -1,7 +1,6 @@
 package spring.contoller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +18,7 @@ public class CarContoller {
     private CarServiceImp carService;
 
     @RequestMapping(value = "/cars")
-    public List<Car> viewCars(@RequestParam(value = "count", required = false)
-                              Optional<Integer> count
+    public List<Car> viewCars(@RequestParam(value = "count", required = false) Optional<Integer> count
             , @RequestParam(value = "sortBy", required = false) String sortBy) {
         return carService.getCars(count.orElse(carService.getFullCar()), sortBy);
     }
